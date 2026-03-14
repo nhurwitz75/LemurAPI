3173ca435dcf0c06a3676bd883387e9e59f34fc9
Nava Hurwitz <navahurwitz@gmail.com>
2026-03-14 23:47:32 +0200

    aadded mood endpoint

--- main.py
+++ main.py
@@ -16,3 +16,9 @@ def get_lemur(id: int):
         if lemur["id"] == id:
             return lemur
     return {"error": "Lemur not found"}, 404
+
+@app.get("/lemurs")
+def get_lemurs(mood: str = None):
+    if mood:
+        return [lemur for lemur in lemurs if lemur["palette"]["mood"].lower() == mood.lower()]
+    return lemurs
